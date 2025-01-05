3ea93682f18c888790293b2014f0769edcee68d3
sahandmoosanezhad <81710657+sahandmoosanezhad@users.noreply.github.com>
2025-01-05 18:47:53 +0330

    Initial commit

--- pandas_lib_exercisee.py
+++ pandas_lib_exercisee.py
@@ -10,3 +10,11 @@ from sqlalchemy import create_engine
 engine = create_engine('sqlite:///:memory:')
 df.to_sql('My_table' ,engine)
 sqldf = pd.read_sql('My_table',con=engine)
+
+# left = pd.DataFrame({'A':['A0','A1','A2'],
+#                     'B':['B0','B1','B2']},
+#                    index=['K0','K1','K2'])
+# right = pd.DataFrame({'C':['C0','C2','C3'],
+#                     'D':['D0','D2','D3']},
+#                    index=['K0','K2','K3'])
+# left.join(right, how='outer')
